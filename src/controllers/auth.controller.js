d33d1e6095512e3d4afddf340e7563787486fd6c
abhishek <shukla13122000@gmail.com>
2024-11-26 21:41:11 +0530

    add user controller and model

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,1 +1,38 @@
+import { User } from "../models/user.model.js";
+import { sign } from "jsonwebtoken";
 
+const expireDuration = 3 * 24 * 60 * 60 * 1000;
+
+const createToken = (email, userId) => {
+  return sign({ email, userId }, process.env.JWT_KEY, {
+    expiresIn: expireDuration,
+  });
+};
+
+export const registerUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(409).send("Email and Password are required");
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).send("User already exists.");
+    }
+
+    const user = await User.create({
+      email,
+      password,
+    });
+    const createdUser = await User.findById(user._id).select("-password");
+    if (!createdUser) {
+      return res.status(500).send("Error: While registering user.");
+    }
+
+    return res.status(201).json();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Error: Could not register user.");
+  }
+};
---
+++ src/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: false,
+    },
+    lastName: {
+      type: String,
+      required: false,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: false,
+    },
+    profileSetup: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+export const User = mongoose.model("User", userSchema);
